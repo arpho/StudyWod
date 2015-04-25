1e181a2e3e570438d0c1e206cb98d449bc07c68a
D'Amico Giuseppe <damicogiuseppe77@gmail.com>
2015-04-25 23:06:36 +0200

    minimal refactoring

--- www/js/services/Utilities.js
+++ www/js/services/Utilities.js
@@ -1,17 +1,28 @@
 angular.module('StudyWod.services', []).factory('Utility',function($window, $ionicLoading){
 	var show = function(text) {
-            $rootScope.loading = $ionicLoading.show({
+            var loading = $ionicLoading.show({
                 content: text ? text : 'Loading..',
                 animation: 'fade-in',
                 showBackdrop: true,
                 maxWidth: 200,
                 showDelay: 0
-            })},
-            hide = function() {
-            $ionicLoading.hide();
-        };
-        return {'show':show,'hide':hide};
+            })}
+            ,hide = function() {
+                $ionicLoading.hide();
+            }
+            ,notify = function(text) {
+        show(text);
+      $window.setTimeout(function() {
+        hide();
+      }, 1999);
+    };
 
-        }
+        return {
+                    'show':show
+                    ,'hide':hide
+                    , 'notiofy':notify
+                };
+
+}
 	
 	);
