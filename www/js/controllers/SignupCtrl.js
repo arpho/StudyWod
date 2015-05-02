99c61ff5a890c70c135821d520a37426b7bb6602
D'Amico Giuseppe <damicogiuseppe77@gmail.com>
2015-05-02 21:59:58 +0200

    Compresi services diangularjs

--- www/js/controllers/SignupCtrl.js
+++ www/js/controllers/SignupCtrl.js
@@ -1,16 +1,20 @@
 angular.module('StudyWod.controllers')
 .controller('SignUpController', [
     '$scope', 'User', '$window',
-    function($scope, $rootScope,$user,  $window) {
+    function($scope, user,  $window) {
         $scope.user = {
             email: "",
             password: ""
         };
         $scope.createUser = function() {
             var email = this.user.email;
+            console.log('createUser');
+            //alert(user.sayHello('popiu'));
             var password = this.user.password;
+            user.setUser(email,password);
             //$user.setUser(this.user.email,this.user.password)
-            alert('signing '+email+' up  with '+password+$user.hw);
+            alert('signing '+user.user+' up  with '+user.password);
+            //console.log(user.sayHello('hello'));
         }
     }
 ])
--- www/js/services/User.js
+++ www/js/services/User.js
@@ -1,15 +1,55 @@
-angular.module('StudyWod.services').factory('User',['Utility', function($firebaseAuth, $firebase){
-	
-	var user ={email:"",
+angular.module('StudyWod.services').service('User',['Utility', function($firebaseAuth, $firebase){
+			this.sayHello = function(text){
+        		return "Service says Hello " + text ;
+    		};
+    		this.setUser = function(user,password){
+    			this.user = user;
+    			this.password = password;
+    		this.createUser = function() {
+            var email = this.user;
+	            var password = this.password;
+	            if (!email || !password) {
+	                Utilities.notify("Please enter valid credentials");
+	                return false;
+            }
+            Utilities.show('Please wait.. Registering');
+
+            auth.$createUser(this.user, this.password, function(error, user) {
+                if (!error) {
+                    Utilities.hide();
+                    $window.location.href = ('#/bucket/list');
+                } else {
+                    Utilities.hide();
+                    if (error.code == 'INVALID_EMAIL') {
+                        Utilities.notify('Invalid Email Address');
+                    } else if (error.code == 'EMAIL_TAKEN') {
+                        Utilities.notify('Email Address already taken');
+                    } else {
+                        Utilities.notify('Oops something went wrong. Please try again later');
+                    }
+                }
+            });
+        }
+    		};
+		}
+	]
+)
+	/*
+var user ={email:"",
 			password:""},
 			baseUrl = 'hhttps://studywod.firebaseio.com//',
-authRef = new Firebase(baseUrl),
-//var auth = $firebaseAuth(authRef),
-setUser = function(email,password){
-		user.email = email;
-		user.password = password;
+	authRef = new Firebase(baseUrl),
+	//var auth = $firebaseAuth(authRef),
+	setUser = function(email,password){
+			user.email = email;
+			user.password = password;
 
-},
+	};
+	return { log: function(msg){
+				alert(msg)
+			}
+	};
+}]);
 createUser = function() {
             var email = user.email;
 	            var password = user.password;
@@ -34,8 +74,10 @@ createUser = function() {
                     }
                 }
             });
-        },
-        hw= ' hello world to ';//+user.email;
-	return {'getUser':user,'setUser':setUser,'createUser':createUser,'hw':hw};
-}]);
+        },*/
+        /*hw= function(){ 
+        	return  'hello world to ' + email;
+        };*/
+	//return {getUser:user,setUser:setUser,/*'createUser':createUser,*/hw:hw};
+//});
 
